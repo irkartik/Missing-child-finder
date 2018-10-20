ec8256a7459aab8bda64addc4d4fef10a3208614
Raju Jha <rajujha373@gmail.com>
2018-10-20 15:30:26 +0530

    shifted logic from file upload to bytes upload

--- core/models.py
+++ core/models.py
@@ -5,15 +5,17 @@ from django.db import models
 class Person(models.Model):
 	name = models.CharField(max_length=100)
 	age = models.IntegerField()
+	fathersName = models.CharField(max_length=100)
 	parentContactNumber = models.CharField(max_length=10, verbose_name="Parent's Contact Number")
 	address = models.TextField()
+	nearest_police_station = models.CharField(max_length=100)
 	height = models.CharField(verbose_name="Height (CM)", max_length=100)
 	weight = models.CharField(verbose_name="Weight (KG)", max_length=100)
-	about = models.TextField(null=True, blank=True)
+	complexion = models.CharField(max_length=100)
 	date_of_missing = models.DateField(blank=True, null=True)
 	place_of_missing = models.CharField(max_length=100, blank=True, null=True)
 	is_missing = models.BooleanField(default=True)
-	image = models.ImageField()
+	image = models.ImageField(null=True, blank= True)
 	created = models.DateTimeField(auto_now_add=True)
 
 	
--- core/views.py
+++ core/views.py
@@ -3,28 +3,34 @@ from django.http import HttpResponse
 # Create your views here.
 from django.views.decorators.csrf import csrf_exempt
 from .models import Person
+from django.core.files.base import ContentFile
 
 import face_recognition
 import numpy as np
 import json
+import base64
+from tempfile import TemporaryFile
+
 
 @csrf_exempt
 def addPerson(request):
 	if request.method == "POST":
 		name = request.POST.get('name')
 		age = request.POST.get('age')
+		fathersName = request.POST.get('father')
+		nearest_police_station = request.POST.get('nearest_police_station')
+		complexion = request.POST.get('complexion')
 		parentContactNumber = request.POST.get('contact')
 		address = request.POST.get('address')
 		height = request.POST.get('height')
 		weight = request.POST.get('weight')
-		about = request.POST.get('about')
-		image = request.FILES['image']
-
-		image1 = face_recognition.load_image_file(image)
-		encoded = face_recognition.face_encodings(image1)[0]
+		image = base64.b64decode(request.POST.get('image')) #request.FILES['image'].encode()))
+		place_of_missing = request.POST.get('place_of_missing')
 
 		try:
-			temp = Person.objects.create(name = name, age = age, parentContactNumber = parentContactNumber, address = address, height = height, weight = weight, about = about, image = image)
+			temp = Person.objects.create(name = name, age = age, parentContactNumber = parentContactNumber, address = address, height = height, weight = weight, place_of_missing = place_of_missing, fathersName = fathersName, nearest_police_station= nearest_police_station, complexion=complexion)
+			temp.image.save('absc.jpeg', ContentFile(image))
+			print(type(ContentFile(image)))
 			temp.save()
 		except Exception as e:
 			return HttpResponse(e)
@@ -33,11 +39,17 @@ def addPerson(request):
 
 @csrf_exempt
 def comparePerson(request):
-	image = request.FILES['image']
+	from django.core.files.uploadedfile import InMemoryUploadedFile
+	from io import BytesIO
 
+	data = base64.b64decode(request.POST.get('image'))
+	print('data type ', type(data))
+	buf = BytesIO(data)
+	buf.seek(0, 2)
+	image = InMemoryUploadedFile(buf, "image", "some_filename.png", None, buf.tell(), None)
+	
 	image_input = face_recognition.load_image_file(image)
 	encoded_input = face_recognition.face_encodings(image_input)[0]
-	print(encoded_input)
 
 	matched = []
 
@@ -51,15 +63,24 @@ def comparePerson(request):
 		result = face_recognition.compare_faces([encoded_target], encoded_input)
 		if result[0]:
 			dic['name'] = person.name
-			dic['parentContactNumber'] = person.parentContactNumber
-			dic['height'] = person.height
-			dic['weight'] = person.weight
+			dic['image_url'] = person.image.url
+			dic['father_name'] = person.fathersName
+			dic['contact'] = person.parentContactNumber
+			dic['age'] = person.age
+			dic['nearest_police_station'] = person.nearest_police_station
 			dic['date_of_missing'] = person.date_of_missing
 			dic['place_of_missing'] = person.place_of_missing
-			dic['is_missing'] = person.is_missing
+			dic['height'] = person.height
+			dic['weight'] = person.weight
+			dic['complexion'] = person.complexion
 			dic['address'] = person.address
-			print('Matched: ' + person.name)
+			#print('Matched: ' + person.name)
 			matched.append(dic)		
-
-	return HttpResponse(json.dumps(matched))
+		final = dict()
+		if len(matched) > 0:
+			final['status'] = 'matched'
+			final['children'] = matched
+		else:
+			final['status'] = 'not found'
+	return HttpResponse(json.dumps(final))
 
